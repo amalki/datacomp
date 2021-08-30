5e66e1cec1c70e7aa9f9c5024fa679dd5efbb18c
Ammar <malkiammar@gmail.com>
2021-08-30 12:13:46 +0100

    switch to pandas instead of lists

--- functions.py
+++ functions.py
@@ -1,6 +1,12 @@
 import csv
+import pandas as pd
 
-def file_to_list(file_path):
+def file_to_df(file_path): #read a file as a pandas dataframe
+    df = pd.read_csv(file_path)
+
+    return df  
+
+def file_to_list(file_path): #read a file as a list of lists
     
     with open(file_path) as file:
         csv_reader = csv.reader(file, delimiter=',')
--- main.py
+++ main.py
@@ -5,14 +5,14 @@ source_file_path = 'input/source/ford_escort.csv'
 target_file_path = 'input/target/ford_escort.csv'
 
 
-source_list = functions.file_to_list(source_file_path)
-target_list = functions.file_to_list(source_file_path)
+source_df = functions.file_to_df(source_file_path)
+target_df = functions.file_to_df(target_file_path)
 
 #comparing source with target
-row_count_source = len(source_list)
-row_count_target = len(target_list)
+row_count_source = source_df.shape[0]
+row_count_target = target_df.shape[0]
 
-print('Does the source and target has the same number of rows?\n' + str(row_count_target==row_count_source))
+print('Does the source and target have the same number of rows?\n' + str(row_count_target==row_count_source))
 
 
 
